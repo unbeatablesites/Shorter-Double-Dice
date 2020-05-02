2fa4bfdca25907df33c1adfdbd17a1f672c5278f
Frank Uzoka <Frankuzoka@hotmail.com>
2020-05-01 23:50:49 -0400

    Add two dice roles

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class App {
 
     public static void main(String[] args) {
@@ -7,6 +9,16 @@ public class App {
     }
 
     static void logic(){
+
+        System.out.println("HERE COME THE DICE!\n");
+
+
+        Random rand = new Random();
+
+        int diceRoleOne = 1 + rand.nextInt(6);
+        int diceRoleTwo = 1 + rand.nextInt(6);
+
+
 
     }
 
