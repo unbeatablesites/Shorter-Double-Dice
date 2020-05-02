7d1ee15adbb879c72b3d88bcb3a63c2246650399
Frank Uzoka <Frankuzoka@hotmail.com>
2020-05-02 00:01:43 -0400

    Add two dice roles

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -12,14 +12,19 @@ public class App {
 
         System.out.println("HERE COME THE DICE!\n");
 
-
         Random rand = new Random();
+        Random rand2 = new Random();
 
-        int diceRoleOne = 1 + rand.nextInt(6);
-        int diceRoleTwo = 1 + rand.nextInt(6);
+        do {
 
-        System.out.println("Roll #1: " + diceRoleOne);
-        System.out.println("Roll #2: " + diceRoleTwo);
+            int diceRoleOne2 = 1 + rand.nextInt(6);
+            int diceRoleTwo1 = 1 + rand2.nextInt(6);
+            System.out.println("Roll #1: " + diceRoleOne2);
+            System.out.println("Roll #2: " + diceRoleTwo1);
+
+        }while (diceRoleOne2 != diceRoleTwo1);
+
+
 
     }
 
